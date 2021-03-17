dd737dd438c80e22b82b2e07de7a44f073a1cec1
CyberUlfr <fodjazz@gmail.com>
2021-03-17 18:51:02 +0700

    Create 1.2.cs

---
+++ 1Lessons/1.2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь состоящий из 10 символов сторон света (Ю,С,З,В):");
+            int x = 0, y = 0;
+            for (int a = 0; a < 10; a++)
+            {
+                char input = Convert.ToChar(Console.Read());
+                Console.Clear();
+                switch (input)
+                {
+                    case 'с':
+                    case 'С':
+                        x++;
+                        break;
+                    case 'з':
+                    case 'З':
+                        y++;
+                        break;
+                    case 'в':
+                    case 'В':
+                        y--;
+                        break;
+                    case 'ю':
+                    case 'Ю':
+                        x--;
+                        break;
+                    default:
+                        Console.WriteLine("Неправильно введены данные");
+                        break;
+                }
+            }
+            double result = Math.Sqrt((x * x) + (y * y));
+            Console.WriteLine($"Растояние между начальной и конченой точкой: {result}");
+            Console.ReadKey();
+        }
+    }
+}
