51e7f1776356c4aeeab08521394e2911ea6041b4
CyberUlfr <fodjazz@gmail.com>
2021-03-18 18:16:24 +0700

    add

---
+++ 1Lessons/1.1(corrected).cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace _1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер месяца:");
+            int monthNumber = int.Parse(Console.ReadLine());
+            switch (monthNumber)
+            {
+                case 1:
+                    Console.WriteLine("Январь");
+                    break;
+                case 2:
+                    Console.WriteLine("Февраль");
+                    break;
+                case 3:
+                    Console.WriteLine("Март");
+                    break;
+                case 4:
+                    Console.WriteLine("Апрель");
+                    break;
+                case 5:
+                    Console.WriteLine("Май");
+                    break;
+                case 6:
+                    Console.WriteLine("Июнь");
+                    break;
+                case 7:
+                    Console.WriteLine("Июль");
+                    break;
+                case 8:
+                    Console.WriteLine("Агуст");
+                    break;
+                case 9:
+                    Console.WriteLine("Сентябрь");
+                    break;
+                case 10:
+                    Console.WriteLine("Октябрь");
+                    break;
+                case 11:
+                    Console.WriteLine("Ноябрь");
+                    break;
+                case 12:
+                    Console.WriteLine("Декабрь");
+                    break;
+                default:
+                    Console.WriteLine("Неправильный номер, введите от 1 до 12");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
