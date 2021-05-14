84fe93ce218252bcc9cc91abfb2ebd1796e91c09
CyberUlfr <fodjazz@gmail.com>
2021-05-14 15:05:19 +0700

    add

--- WindowsFormsTravelOrginizer/WindowsFormsTravelOrginizer/Form1.Designer.cs
+++ WindowsFormsTravelOrginizer/WindowsFormsTravelOrginizer/Form1.Designer.cs
@@ -33,12 +33,19 @@ namespace WindowsFormsTravelOrginizer
             this.listBoxUsers = new System.Windows.Forms.ListBox();
             this.labelText1 = new System.Windows.Forms.Label();
             this.buttonUpdate = new System.Windows.Forms.Button();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.tabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
+            this.tabSelectUser = new System.Windows.Forms.TabControl();
+            this.tabUsers = new System.Windows.Forms.TabPage();
+            this.buttonUsersSelect = new System.Windows.Forms.Button();
+            this.tabMenuAdmin = new System.Windows.Forms.TabPage();
+            this.buttonAddUser = new System.Windows.Forms.Button();
+            this.buttonDeleateUser = new System.Windows.Forms.Button();
+            this.UserUpdate = new System.Windows.Forms.Button();
+            this.listBoxMenuAdminUsers = new System.Windows.Forms.ListBox();
+            this.tabSelectedUser = new System.Windows.Forms.TabPage();
+            this.textBoxUser = new System.Windows.Forms.TextBox();
+            this.tabSelectUser.SuspendLayout();
+            this.tabUsers.SuspendLayout();
+            this.tabMenuAdmin.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonExit
@@ -56,7 +63,7 @@ namespace WindowsFormsTravelOrginizer
             this.listBoxUsers.FormattingEnabled = true;
             this.listBoxUsers.Location = new System.Drawing.Point(6, 7);
             this.listBoxUsers.Name = "listBoxUsers";
-            this.listBoxUsers.Size = new System.Drawing.Size(303, 186);
+            this.listBoxUsers.Size = new System.Drawing.Size(450, 264);
             this.listBoxUsers.TabIndex = 1;
             this.listBoxUsers.SelectedIndexChanged += new System.EventHandler(this.listBoxSelectedName);
             // 
@@ -73,7 +80,7 @@ namespace WindowsFormsTravelOrginizer
             // 
             // buttonUpdate
             // 
-            this.buttonUpdate.Location = new System.Drawing.Point(317, 170);
+            this.buttonUpdate.Location = new System.Drawing.Point(462, 247);
             this.buttonUpdate.Name = "buttonUpdate";
             this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
             this.buttonUpdate.TabIndex = 3;
@@ -81,61 +88,128 @@ namespace WindowsFormsTravelOrginizer
             this.buttonUpdate.UseVisualStyleBackColor = true;
             this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
             // 
-            // tabControl1
+            // tabSelectUser
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Controls.Add(this.tabPage3);
-            this.tabControl1.Location = new System.Drawing.Point(41, 108);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(406, 225);
-            this.tabControl1.TabIndex = 4;
+            this.tabSelectUser.Controls.Add(this.tabUsers);
+            this.tabSelectUser.Controls.Add(this.tabMenuAdmin);
+            this.tabSelectUser.Controls.Add(this.tabSelectedUser);
+            this.tabSelectUser.Location = new System.Drawing.Point(41, 108);
+            this.tabSelectUser.Name = "tabSelectUser";
+            this.tabSelectUser.SelectedIndex = 0;
+            this.tabSelectUser.Size = new System.Drawing.Size(560, 303);
+            this.tabSelectUser.TabIndex = 4;
             // 
-            // tabPage1
+            // tabUsers
             // 
-            this.tabPage1.Controls.Add(this.listBoxUsers);
-            this.tabPage1.Controls.Add(this.buttonUpdate);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(398, 199);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabUsers.Controls.Add(this.buttonUsersSelect);
+            this.tabUsers.Controls.Add(this.listBoxUsers);
+            this.tabUsers.Controls.Add(this.buttonUpdate);
+            this.tabUsers.Location = new System.Drawing.Point(4, 22);
+            this.tabUsers.Name = "tabUsers";
+            this.tabUsers.Padding = new System.Windows.Forms.Padding(3);
+            this.tabUsers.Size = new System.Drawing.Size(552, 277);
+            this.tabUsers.TabIndex = 0;
+            this.tabUsers.Text = "Выбор пользователя";
+            this.tabUsers.UseVisualStyleBackColor = true;
+            this.tabUsers.Click += new System.EventHandler(this.tabUsers_Click);
             // 
-            // tabPage2
+            // buttonUsersSelect
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(398, 199);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.buttonUsersSelect.Location = new System.Drawing.Point(462, 218);
+            this.buttonUsersSelect.Name = "buttonUsersSelect";
+            this.buttonUsersSelect.Size = new System.Drawing.Size(75, 23);
+            this.buttonUsersSelect.TabIndex = 4;
+            this.buttonUsersSelect.Text = "Выбор";
+            this.buttonUsersSelect.UseVisualStyleBackColor = true;
+            this.buttonUsersSelect.Click += new System.EventHandler(this.buttonUsersSelect_Click);
             // 
-            // tabPage3
+            // tabMenuAdmin
             // 
-            this.tabPage3.Location = new System.Drawing.Point(4, 22);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(192, 199);
-            this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "tabPage3";
-            this.tabPage3.UseVisualStyleBackColor = true;
+            this.tabMenuAdmin.Controls.Add(this.textBoxUser);
+            this.tabMenuAdmin.Controls.Add(this.buttonAddUser);
+            this.tabMenuAdmin.Controls.Add(this.buttonDeleateUser);
+            this.tabMenuAdmin.Controls.Add(this.UserUpdate);
+            this.tabMenuAdmin.Controls.Add(this.listBoxMenuAdminUsers);
+            this.tabMenuAdmin.Location = new System.Drawing.Point(4, 22);
+            this.tabMenuAdmin.Name = "tabMenuAdmin";
+            this.tabMenuAdmin.Padding = new System.Windows.Forms.Padding(3);
+            this.tabMenuAdmin.Size = new System.Drawing.Size(552, 277);
+            this.tabMenuAdmin.TabIndex = 1;
+            this.tabMenuAdmin.Text = "Меню админа";
+            this.tabMenuAdmin.UseVisualStyleBackColor = true;
+            this.tabMenuAdmin.Click += new System.EventHandler(this.tabMenuAdmin_Click);
+            // 
+            // buttonAddUser
+            // 
+            this.buttonAddUser.Location = new System.Drawing.Point(471, 33);
+            this.buttonAddUser.Name = "buttonAddUser";
+            this.buttonAddUser.Size = new System.Drawing.Size(75, 23);
+            this.buttonAddUser.TabIndex = 6;
+            this.buttonAddUser.Text = "Добавить";
+            this.buttonAddUser.UseVisualStyleBackColor = true;
+            this.buttonAddUser.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // buttonDeleateUser
+            // 
+            this.buttonDeleateUser.Location = new System.Drawing.Point(471, 62);
+            this.buttonDeleateUser.Name = "buttonDeleateUser";
+            this.buttonDeleateUser.Size = new System.Drawing.Size(75, 23);
+            this.buttonDeleateUser.TabIndex = 5;
+            this.buttonDeleateUser.Text = "Удалить";
+            this.buttonDeleateUser.UseVisualStyleBackColor = true;
+            this.buttonDeleateUser.Click += new System.EventHandler(this.buttonDeleateUser_Click);
+            // 
+            // UserUpdate
+            // 
+            this.UserUpdate.Location = new System.Drawing.Point(471, 248);
+            this.UserUpdate.Name = "UserUpdate";
+            this.UserUpdate.Size = new System.Drawing.Size(75, 23);
+            this.UserUpdate.TabIndex = 4;
+            this.UserUpdate.Text = "Обновить";
+            this.UserUpdate.UseVisualStyleBackColor = true;
+            this.UserUpdate.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // listBoxMenuAdminUsers
+            // 
+            this.listBoxMenuAdminUsers.FormattingEnabled = true;
+            this.listBoxMenuAdminUsers.Location = new System.Drawing.Point(6, 7);
+            this.listBoxMenuAdminUsers.Name = "listBoxMenuAdminUsers";
+            this.listBoxMenuAdminUsers.Size = new System.Drawing.Size(363, 264);
+            this.listBoxMenuAdminUsers.TabIndex = 2;
+            this.listBoxMenuAdminUsers.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
+            // 
+            // tabSelectedUser
+            // 
+            this.tabSelectedUser.Location = new System.Drawing.Point(4, 22);
+            this.tabSelectedUser.Name = "tabSelectedUser";
+            this.tabSelectedUser.Padding = new System.Windows.Forms.Padding(3);
+            this.tabSelectedUser.Size = new System.Drawing.Size(552, 277);
+            this.tabSelectedUser.TabIndex = 2;
+            this.tabSelectedUser.Text = "Меню пользователя";
+            this.tabSelectedUser.UseVisualStyleBackColor = true;
+            // 
+            // textBoxUser
+            // 
+            this.textBoxUser.Location = new System.Drawing.Point(375, 7);
+            this.textBoxUser.Name = "textBoxUser";
+            this.textBoxUser.Size = new System.Drawing.Size(171, 20);
+            this.textBoxUser.TabIndex = 7;
+            this.textBoxUser.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.tabSelectUser);
             this.Controls.Add(this.labelText1);
             this.Controls.Add(this.buttonExit);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.tabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
+            this.tabSelectUser.ResumeLayout(false);
+            this.tabUsers.ResumeLayout(false);
+            this.tabMenuAdmin.ResumeLayout(false);
+            this.tabMenuAdmin.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -147,10 +221,16 @@ namespace WindowsFormsTravelOrginizer
         private System.Windows.Forms.ListBox listBoxUsers;
         private System.Windows.Forms.Label labelText1;
         private System.Windows.Forms.Button buttonUpdate;
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabControl tabSelectUser;
+        private System.Windows.Forms.TabPage tabUsers;
+        private System.Windows.Forms.TabPage tabMenuAdmin;
+        private System.Windows.Forms.TabPage tabSelectedUser;
+        private System.Windows.Forms.Button buttonUsersSelect;
+        private System.Windows.Forms.Button UserUpdate;
+        private System.Windows.Forms.ListBox listBoxMenuAdminUsers;
+        private System.Windows.Forms.Button buttonAddUser;
+        private System.Windows.Forms.Button buttonDeleateUser;
+        private System.Windows.Forms.TextBox textBoxUser;
     }
 }
 
--- WindowsFormsTravelOrginizer/WindowsFormsTravelOrginizer/Form1.cs
+++ WindowsFormsTravelOrginizer/WindowsFormsTravelOrginizer/Form1.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using travelOrganizer;
 
 namespace WindowsFormsTravelOrginizer
 {
@@ -27,17 +30,7 @@ namespace WindowsFormsTravelOrginizer
         }
         private void listBoxSelectedName(object sender, EventArgs e)
         {
-            foreach (var user in travelOrganizer.User.Users)
-            {
-                if (!user.IsAdmin)
-                {
-                    
-                }
-                else
-                {
-                    var admin = travelOrganizer.User.Users.First(username => username == user);
-                }
-            }
+            
 
         }
 
@@ -49,6 +42,68 @@ namespace WindowsFormsTravelOrginizer
         }
 
         private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+
+        }
+
+        private void buttonUsersSelect_Click(object sender, EventArgs e)
+        {
+            foreach (var user in travelOrganizer.User.Users)
+            {
+                if (!user.IsAdmin)
+                {
+                    tabSelectUser.SelectedIndex = 1;
+                }
+                else
+                {
+                    tabSelectUser.SelectedIndex = 2;
+                }
+            }
+        }
+
+        private void tabUsers_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabMenuAdmin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            listBoxMenuAdminUsers.Items.Clear();
+            foreach (var user in travelOrganizer.User.Users)
+                listBoxMenuAdminUsers.Items.Add(user.Name);
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            if (textBoxUser.Text.Trim() == "")
+            {
+                MessageBox.Show("Не все поля заполнены!", "Ошибка!");
+                return;
+            }
+            string userName = textBoxUser.Text.Trim();
+            User.Users.Add(new User(userName));
+        }
+        private void buttonDeleateUser_Click(object sender, EventArgs e)
+        {
+            if ((User)listBoxMenuAdminUsers.SelectedItem == null)
+            {
+                MessageBox.Show("Выберите пользователя для удаления!", "Ошибка выбора!");
+                return;
+            }
+            User.Users.Remove((User)listBoxMenuAdminUsers.SelectedItem);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
